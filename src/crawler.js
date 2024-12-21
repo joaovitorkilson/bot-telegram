7de7eb5cd885232914a0c4b31e1376ad0171de0a
joaovitorkilson <joaovitorkilson@gmail.com>
2024-12-20 23:33:04 -0300

    fix executablePath

--- src/crawler.js
+++ src/crawler.js
@@ -1,4 +1,4 @@
-import puppeteer from 'puppeteer-core'
+import puppeteer from 'puppeteer'
 
 
 
@@ -7,6 +7,7 @@ export async function getPixCode(payment_url) {
   try {
     // Inicializa o navegador
     browser = await puppeteer.launch({
+      executablePath: '/opt/render/project/puppeteer/chrome/linux-131.0.6778.204/chrome-linux64/chrome',
       headless: true, // Certifique-se de rodar no modo headless em produção
       args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--disable-gpu'], // Opções para ambientes como Docker
     });
